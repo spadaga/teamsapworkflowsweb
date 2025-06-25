0d73a968954844ba789c037f0a0a6373925a3cd5
gpadaga <padagatools@gmail.com>
2025-06-25 14:38:34 +0530

    error fix

--- src/sapConnection.js
+++ src/sapConnection.js
@@ -89,11 +89,11 @@ export async function fetchSAPWorkflows() {
 
         // Fetch mock data from mockWorkflows.json
         //const mockResponse = await fetch('/mockWorkflows.json');
-       // const mockData =[]// await mockResponse.json(); //[];//
+        const mockData =[]// await mockResponse.json(); //[];//
        // console.log('Fetched mock data:', mockData);
 
        let mockWorkflows = [];
-console.log('Mock data fetch skipped or failed, using empty array');
+console.log('Mock data fetch skipped or failed, using empty array');    
         if (Array.isArray(mockData)) {
             mockWorkflows = mockData;
         } else {
